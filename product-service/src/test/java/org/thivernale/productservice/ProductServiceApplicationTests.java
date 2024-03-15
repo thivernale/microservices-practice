836377f7db41ff5f8b3911b457cf2ac1fa336066
thivernale <thivernale@gmail.com>
2024-03-15 10:57:13 +0200

    Product Service - initial xommit

---
+++ product-service/src/test/java/org/thivernale/productservice/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.thivernale.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

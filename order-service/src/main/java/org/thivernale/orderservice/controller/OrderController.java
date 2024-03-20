1ce6bbb051e199cf7c11e0cb684878cd148be7d6
thivernale <thivernale@gmail.com>
2024-03-20 15:41:41 +0200

    Implement Circuit Breaker and various scenarios for timeout and retry

--- order-service/src/main/java/org/thivernale/orderservice/controller/OrderController.java
+++ order-service/src/main/java/org/thivernale/orderservice/controller/OrderController.java
@@ -1,6 +1,10 @@
 package org.thivernale.orderservice.controller;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.thivernale.orderservice.dto.OrderRequest;
@@ -8,18 +12,31 @@ import org.thivernale.orderservice.dto.OrderResponse;
 import org.thivernale.orderservice.service.OrderService;
 
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 @RestController
 @RequestMapping("/api/order")
 @RequiredArgsConstructor
+@Slf4j
 public class OrderController {
 
     private final OrderService orderService;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public void placeOrder(@RequestBody OrderRequest orderRequest) {
-        orderService.placeOrder(orderRequest);
+    @CircuitBreaker(name = "inventory", fallbackMethod = "fallbackMethod")
+    @TimeLimiter(name = "inventory")
+    @Retry(name = "inventory")
+    public CompletableFuture<?> placeOrder(@RequestBody OrderRequest orderRequest) {
+        return CompletableFuture.supplyAsync(() -> {
+            orderService.placeOrder(orderRequest);
+            return null;
+        });
+    }
+
+    public CompletableFuture<?> fallbackMethod(OrderRequest orderRequest, RuntimeException exception) {
+        log.warn(exception.getMessage());
+        return CompletableFuture.supplyAsync(() -> null);
     }
 
     @GetMapping

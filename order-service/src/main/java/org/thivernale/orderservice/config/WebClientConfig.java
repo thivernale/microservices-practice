28316af6621168eb4fd0bee364d4f918eb680a92
thivernale <thivernale@gmail.com>
2024-04-01 12:19:03 +0300

    RestClient.Builder configuration

--- order-service/src/main/java/org/thivernale/orderservice/config/WebClientConfig.java
+++ order-service/src/main/java/org/thivernale/orderservice/config/WebClientConfig.java
@@ -1,15 +1,31 @@
 package org.thivernale.orderservice.config;
 
+import org.springframework.boot.autoconfigure.web.client.RestClientBuilderConfigurer;
+import org.springframework.boot.web.client.ClientHttpRequestFactories;
+import org.springframework.boot.web.client.ClientHttpRequestFactorySettings;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestClient;
 
+import java.time.Duration;
+
 @Configuration
 public class WebClientConfig {
+    /**
+     * There is an
+     * {@link org.springframework.boot.autoconfigure.web.client.RestClientAutoConfiguration#restClientBuilder(RestClientBuilderConfigurer) autoconfigured} bean
+     *
+     * @param restClientBuilderConfigurer
+     * @return
+     */
     @Bean
     @LoadBalanced
-    public RestClient.Builder restClientBuilder() {
-        return RestClient.builder();
+    public RestClient.Builder restClientBuilder(RestClientBuilderConfigurer restClientBuilderConfigurer) {
+        return restClientBuilderConfigurer.configure(RestClient.builder()
+            .requestFactory(ClientHttpRequestFactories.get(ClientHttpRequestFactorySettings.DEFAULTS
+                .withConnectTimeout(Duration.ofSeconds(5))
+                .withReadTimeout(Duration.ofSeconds(5)))))
+            ;
     }
 }

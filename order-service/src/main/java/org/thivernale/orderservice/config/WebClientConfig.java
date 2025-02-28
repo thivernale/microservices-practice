065fd2be1897655186fcf33171d845b17027c3cd
thivernale <thivernale@gmail.com>
2025-02-28 09:50:43 +0200

    Replace use of deprecated classes in RestClient.Builder configuration bean

--- order-service/src/main/java/org/thivernale/orderservice/config/WebClientConfig.java
+++ order-service/src/main/java/org/thivernale/orderservice/config/WebClientConfig.java
@@ -1,8 +1,9 @@
 package org.thivernale.orderservice.config;
 
+import org.springframework.boot.autoconfigure.web.client.RestClientAutoConfiguration;
 import org.springframework.boot.autoconfigure.web.client.RestClientBuilderConfigurer;
-import org.springframework.boot.web.client.ClientHttpRequestFactories;
-import org.springframework.boot.web.client.ClientHttpRequestFactorySettings;
+import org.springframework.boot.http.client.ClientHttpRequestFactoryBuilder;
+import org.springframework.boot.http.client.ClientHttpRequestFactorySettings;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,19 +14,15 @@ import java.time.Duration;
 @Configuration
 public class WebClientConfig {
     /**
-     * There is an
-     * {@link org.springframework.boot.autoconfigure.web.client.RestClientAutoConfiguration#restClientBuilder(RestClientBuilderConfigurer) autoconfigured} bean
-     *
-     * @param restClientBuilderConfigurer
-     * @return
+     * See {@link RestClientAutoConfiguration} autoconfigured bean
      */
     @Bean
     @LoadBalanced
     public RestClient.Builder restClientBuilder(RestClientBuilderConfigurer restClientBuilderConfigurer) {
         return restClientBuilderConfigurer.configure(RestClient.builder()
-            .requestFactory(ClientHttpRequestFactories.get(ClientHttpRequestFactorySettings.DEFAULTS
-                .withConnectTimeout(Duration.ofSeconds(5))
-                .withReadTimeout(Duration.ofSeconds(5)))))
-            ;
+            .requestFactory(ClientHttpRequestFactoryBuilder.detect()
+                .build(ClientHttpRequestFactorySettings.defaults()
+                    .withConnectTimeout(Duration.ofSeconds(5))
+                    .withReadTimeout(Duration.ofSeconds(5)))));
     }
 }

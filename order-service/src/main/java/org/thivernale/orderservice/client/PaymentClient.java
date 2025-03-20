ab87a9995489a52b069ce982fecc0dfdc1e0aaa5
thivernale <thivernale@gmail.com>
2025-03-20 22:36:33 +0200

    Add url configuration for payment and customer HTTP clients

--- order-service/src/main/java/org/thivernale/orderservice/client/PaymentClient.java
+++ order-service/src/main/java/org/thivernale/orderservice/client/PaymentClient.java
@@ -5,7 +5,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.thivernale.orderservice.dto.PaymentRequest;
 
-@FeignClient(name = "payment-service", path = "/api/payment")
+@FeignClient(
+    name = "payment-service",
+    path = "/api/payment",
+    url = "${app.urls.payment-service:http://payment-service}"
+)
 public interface PaymentClient {
     @PostMapping
     Long createPayment(@RequestBody PaymentRequest paymentRequest);

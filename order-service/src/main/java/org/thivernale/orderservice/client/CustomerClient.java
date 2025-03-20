07ecc109bcc30073e08c562d8dac08aeee38499f
thivernale <thivernale@gmail.com>
2025-03-20 22:36:33 +0200

    Add url configuration for payment and customer HTTP clients

--- order-service/src/main/java/org/thivernale/orderservice/client/CustomerClient.java
+++ order-service/src/main/java/org/thivernale/orderservice/client/CustomerClient.java
@@ -7,7 +7,11 @@ import org.thivernale.orderservice.dto.CustomerResponse;
 
 import java.util.Optional;
 
-@FeignClient(name = "customer-service", path = "/api/customer/")
+@FeignClient(
+    name = "customer-service",
+    path = "/api/customer/",
+    url = "${app.urls.customer-service:http://customer-service}"
+)
 public interface CustomerClient {
     @GetMapping("{id}")
     Optional<CustomerResponse> findById(@PathVariable String id);

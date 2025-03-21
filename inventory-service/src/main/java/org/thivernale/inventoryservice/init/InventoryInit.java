888b94b8a6d8ecfd8018c1fde34337cc02b405c9
thivernale <thivernale@gmail.com>
2025-03-21 13:12:02 +0200

    Add condition for initialization of inventory items

--- inventory-service/src/main/java/org/thivernale/inventoryservice/init/InventoryInit.java
+++ inventory-service/src/main/java/org/thivernale/inventoryservice/init/InventoryInit.java
@@ -1,6 +1,7 @@
 package org.thivernale.inventoryservice.init;
 
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.thivernale.inventoryservice.model.Inventory;
@@ -11,6 +12,11 @@ import java.util.List;
 @Configuration
 public class InventoryInit {
     @Bean
+    @ConditionalOnProperty(
+        prefix = "app.datasource",
+        value = "init",
+        havingValue = "true"
+    )
     public CommandLineRunner loadData(InventoryRepository inventoryRepository) {
         return args -> {
             if (0 == inventoryRepository.count()) {

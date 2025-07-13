54173e9a94beba3294c9d082f7251bb0f50ca694
thivernale <thivernale@gmail.com>
2025-07-13 23:27:07 +0300

    Add error handler for protobuf consumer and for deserialization errors in json-value consumer

--- notification-service/src/main/java/org/thivernale/notificationservice/config/KafkaConsumerConfig.java
+++ notification-service/src/main/java/org/thivernale/notificationservice/config/KafkaConsumerConfig.java
@@ -1,7 +1,10 @@
 package org.thivernale.notificationservice.config;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.Consumer;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.common.errors.RecordDeserializationException;
 import org.apache.kafka.common.serialization.ByteArrayDeserializer;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.beans.factory.annotation.Value;
@@ -11,6 +14,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.listener.CommonErrorHandler;
+import org.springframework.kafka.listener.KafkaListenerErrorHandler;
+import org.springframework.kafka.listener.MessageListenerContainer;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
 
 import java.util.HashMap;
@@ -74,13 +80,57 @@ public class KafkaConsumerConfig {
 
     @Bean
     public ConcurrentKafkaListenerContainerFactory<String, Object> protobufListenerContainerFactory(
-        final ConsumerFactory<String, Object> protobufConsumerFactory
-        /*, CommonErrorHandler commonErrorHandler*/
+        final ConsumerFactory<String, Object> protobufConsumerFactory,
+        final CommonErrorHandler commonErrorHandler
     ) {
         ConcurrentKafkaListenerContainerFactory<String, Object> factory =
             new ConcurrentKafkaListenerContainerFactory<>();
         factory.setConsumerFactory(protobufConsumerFactory);
-        //factory.setCommonErrorHandler(commonErrorHandler);
+        factory.setCommonErrorHandler(commonErrorHandler);
         return factory;
+    }
+
+    @Bean
+    CommonErrorHandler commonErrorHandler() {
+        return new CommonErrorHandler() {
+            @Override
+            public boolean handleOne(
+                Exception thrownException,
+                ConsumerRecord<?, ?> record,
+                Consumer<?, ?> consumer,
+                MessageListenerContainer container
+            ) {
+//                return CommonErrorHandler.super.handleOne(thrownException, record, consumer, container);
+                handle(thrownException, consumer);
+                return true;
+            }
+
+            @Override
+            public void handleOtherException(
+                Exception thrownException,
+                Consumer<?, ?> consumer,
+                MessageListenerContainer container,
+                boolean batchListener
+            ) {
+//                CommonErrorHandler.super.handleOtherException(thrownException, consumer, container, batchListener);
+                handle(thrownException, consumer);
+            }
+
+            void handle(Exception exception, Consumer<?, ?> consumer) {
+                if (exception instanceof RecordDeserializationException ex) {
+                    consumer.seek(ex.topicPartition(), ex.offset() + 1L);
+                    consumer.commitSync();
+                }
+                log.error(exception.getMessage());
+            }
+        };
+    }
+
+    @Bean
+    KafkaListenerErrorHandler kafkaListenerErrorHandler() {
+        return (message, exception) -> {
+            log.error(exception.getMessage());
+            return null;
+        };
     }
 }

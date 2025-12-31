9b85e616175a6bf18250c0669d3fd66636cd0db9
thivernale <thivernale@gmail.com>
2025-12-31 19:49:38 +0200

    Generate API for calling backend

--- order-ui/src/app/app.config.ts
+++ order-ui/src/app/app.config.ts
@@ -4,19 +4,20 @@ import {provideRouter} from '@angular/router';
 import {routes} from './app.routes';
 import {providePrimeNG} from 'primeng/config';
 import Aura from '@primeng/themes/aura';
-import {provideHttpClient} from '@angular/common/http';
+import {provideHttpClient, withInterceptors} from '@angular/common/http';
 import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
+import {provideApiConfiguration as provideApiConfigurationProduct} from './services/product/api-configuration';
+
+// TODO externalize rootUrl of API-Gateway service
+const API_ROOT_URL = 'http://localhost:8080';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({eventCoalescing: true}),
     provideRouter(routes),
-    provideHttpClient(),
+    provideHttpClient(withInterceptors([])),
+    provideApiConfigurationProduct(`${API_ROOT_URL}`),
     provideAnimationsAsync(),
-    providePrimeNG({
-      theme: {
-        preset: Aura
-      }
-    })
+    providePrimeNG({theme: {preset: Aura}}),
   ]
 };

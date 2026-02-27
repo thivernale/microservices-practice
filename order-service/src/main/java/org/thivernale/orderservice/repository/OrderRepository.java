042f5195ca65b47e149a5badcc8ccdd4830a1886
thivernale <thivernale@gmail.com>
2026-02-27 19:22:16 +0200

    WIP JPA Query methods and optimization research

--- order-service/src/main/java/org/thivernale/orderservice/repository/OrderRepository.java
+++ order-service/src/main/java/org/thivernale/orderservice/repository/OrderRepository.java
@@ -1,7 +1,21 @@
 package org.thivernale.orderservice.repository;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.lang.NonNull;
 import org.thivernale.orderservice.model.Order;
 
+import java.util.List;
+
 public interface OrderRepository extends JpaRepository<Order, Long> {
+    @Override
+    @EntityGraph(attributePaths = {"items"})
+    @NonNull
+    List<Order> findAll();
+
+    @EntityGraph(attributePaths = {"items"})
+    @Query("SELECT o FROM Order o")
+        // Explicitly define the query
+    List<Order> findAllWithItems();
 }
